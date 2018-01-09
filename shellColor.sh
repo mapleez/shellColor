8e7c70734d34312b59b4ebc467c55af125c4089f
mapleez <kukunanhai_5207@126.com>
2018-01-09 14:03:45 +0800

    Display the remaining 2 styles for each background color.

--- shellColor.sh
+++ shellColor.sh
@@ -2,7 +2,7 @@
 
 # Editor : ez
 # Date : 2018/1/9
-# CHANGE : Add some comments.
+# CHANGE : Display the remaining 2 styles.
 
 ## bash script with color in terminal
 # echo -e \033[style;colorm : with style
@@ -37,24 +37,28 @@ colors=( "blk" "red" "grn" "yel" "blu" "mag" "cyn" "wht" )
 color_num=${#colors[@]}
 
 ## Print table
-# Each row will have two lines : Normal and Bold style, and will represent a background colors
+# Each row will have 4 lines : Normal, Bold, Underlined and Blinking style, and will represent a background colors
 # Each column is a foreground color
 for (( row = -1; row < $color_num; row++ )); do
     # Start line
     if [ $row -eq -1 ]; then
         # Start default line (line -1)
-        # Initialize style Bolod (1) on second line
+        # Initialize 4 style on each line
         line1="DFT :  ";
         line2="    : \033[1m ";
+        line3="    : \033[4m ";
+        line4="    : \033[5m ";
     else
         # Start colored line (line 0-7)
         # Search color name
         name=${colors[row]}
 
         # Initialize lines start and foreground (4* color)
-        # Initialize style Bolod (1) on second line
+        # Initialize 4 style on each line
         line1="$(echo $name | tr 'a-z' 'A-Z') : \033[4${row}m ";
         line2="    : \033[1;4${row}m ";
+        line3="    : \033[4;4${row}m ";
+        line4="    : \033[5;4${row}m ";
     fi
 
     # Fill lines's columns
@@ -63,15 +67,21 @@ for (( row = -1; row < $color_num; row++ )); do
         column="  \033[3${color}m${colors[color]}  "
         line1+=$column;
         line2+=$column;
+				line3+=$column;
+				line4+=$column;
     done
 
-    # Reset settings (style bold (1), foreground (3*) and background (4*))
+    # Reset settings (style, foreground (3*) and background (4*))
     line1+=" \033[39;49m :"; # reset only colors
     line2+=" \033[0;39;49m :"; # reset colors and style
+    line3+=" \033[0;39;49m :"; # reset colors and style
+    line4+=" \033[0;39;49m :"; # reset colors and style
 
     # Print separator and line
     echo -e "    --------------------------------------------------------------"
     echo -e "$line1"
     echo -e "$line2"
+    echo -e "$line3"
+    echo -e "$line4"
 done
 echo -e "    --------------------------------------------------------------"
